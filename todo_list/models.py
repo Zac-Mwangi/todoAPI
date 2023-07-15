db6855d173f697c0c084c9d0e1f01561d90f6c65
ragnar <zackmwangi998@gmail.com>
2023-07-15 04:36:11 +0300

    desc

--- todo_list/models.py
+++ todo_list/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Todo(models.Model):
     title = models.CharField(max_length=255)
+    desc = models.CharField(max_length=255,null=True, blank=True)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
